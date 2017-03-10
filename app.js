7641c620f119c311f7d65a8a5b956ec01f6efd0f
Will Hawkins <willhawkins123@yahoo.com>
2017-03-10 15:05:16 -0500

    Angular Front End Fully Visible but no Factory yet

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 angular
-  .module("superApop", [
+  .module("superApp", [
     "ui.router",
     "ngResource"
   ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .factory(["ActivityFactory",
+  "$resource",
+  ActivityFactoryFunction
+  ])
+  .controller(["ActivityIndexController",
+  "ActivityFactory",
+  ActivityIndexControllerFunction
+  ])
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+      .state("activityIndex", {
+        url: "",
+        templateUrl: "ng-views/index.html",
+        controllerAs: "vm"
+      })
+  }
+
+  function ActivityFactoryFunction($resource){
+    return $resource("", {}, {
+      update: {method: "PUT"}
+    })
+  }
+
+  function ActivityIndexControllerFunction(ActivityFactory, $state){
+    this.activities = ActivityFactory.query();
+
+    this.activity = new ActivityFactory();
+    this.create = function(){
+      this.activity.$save(function(activity) {
+        $state.go("activityIndex")
+      })
+    }
+  }
