e135425059331f426bbf2fcb630e638430b6ef91
Will Hawkins <willhawkins123@yahoo.com>
2017-03-11 16:54:24 -0500

    ActivityNewController and Function added as well as View but go isn't working

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ angular
   "ActivityFactory",
   ActivityIndexControllerFunction
   ])
+  .controller("ActivityNewController", [
+    "ActivityFactory",
+    ActivityNewControllerFunction
+  ])
 
   function RouterFunction($stateProvider){
     $stateProvider
@@ -24,17 +28,25 @@ angular
         controller: "ActivityIndexController",
         controllerAs: "vm"
       })
+      .state("activityNew", {
+        url: "/activities/new",
+        templateUrl: "ng-views/new.html",
+        controller: "ActivityNewController",
+        controllerAs: "vm"
+      })
   }
 
   function ActivityFactoryFunction($resource){
-    return  $resource("http://localhost:3000/activities/:id", {}, {
+    return  $resource("http://localhost:3000/activities/:idI ", {}, {
         update: {method: "PUT"}
       })
   }
 
   function ActivityIndexControllerFunction(ActivityFactory, $state){
     this.activities = ActivityFactory.query();
+  }
 
+  function ActivityNewControllerFunction(ActivityFactory, $state){
     this.activity = new ActivityFactory();
     this.create = function(){
       this.activity.$save(function(activity) {
