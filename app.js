a4571601396676f475fc287018a35aa82022687e
Will Hawkins <willhawkins123@yahoo.com>
2017-03-12 13:53:43 -0400

    Typo Fixes to remedy edit link issues

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"
 
 angular
   .module("superApp", [
@@ -21,7 +21,7 @@ angular
     "ActivityFactory",
     ActivityNewControllerFunction
   ])
-  .controller("ActivityShowControler", [
+  .controller("ActivityShowController", [
   "ActivityFactory",
   "$stateParams",
   ActivityShowControllerFunction
@@ -31,7 +31,7 @@ angular
     "$stateParams",
     "$state",
     ActivityEditControllerFunction
-  ]);
+  ])
 
   function RouterFunction($stateProvider){
     $stateProvider
@@ -50,45 +50,46 @@ angular
       .state("activityShow", {
         url: "/activities/:id",
         templateUrl: "ng-views/show.html",
-        controller: "ActivityShowControler",
+        controller: "ActivityShowController",
         controllerAs: "vm"
       })
       .state("activityEdit", {
         url: "/activities/:id/edit",
         templateUrl: "ng-views/edit.html",
         controller: "ActivityEditController",
-      });
+        controllerAs: "vm"
+      })
   }
 
   function ActivityFactoryFunction($resource){
     return  $resource("http://localhost:3000/activities/:id ", {}, {
         update: {method: "PUT"}
-      });
+      })
   }
 
   function ActivityIndexControllerFunction(ActivityFactory, $state){
-    this.activities = ActivityFactory.query();
+    this.activities = ActivityFactory.query()
   }
 
   function ActivityNewControllerFunction(ActivityFactory, $state){
-    this.activity = new ActivityFactory();
+    this.activity = new ActivityFactory()
     this.create = function(){
       this.activity.$save(function(activity) {
-        $state.go("activityIndex"); 
-      });
-    };
+        $state.go("activityIndex")
+      })
+    }
   }
 
   function ActivityShowControllerFunction(ActivityFactory, $stateParams){
-    this.activity = ActivityFactory.get({id: $stateParams.id});
+    this.activity = ActivityFactory.get({id: $stateParams.id})
  }
 
   function ActivityEditControllerFunction( ActivityFactory, $stateParams , $state){
-    this.activity = ActivityFactory.get({id: $stateParams.id});
+    this.activity = ActivityFactory.get({id: $stateParams.id})
     this.update = function(){
       this.activity.$update({id: $stateParams.id},
         function(activity) {
-        $state.go("activityShow", {id: activity.id});
-      });
-    };
+        $state.go("activityShow", {id: activity.id})
+      })
+    }
   }
